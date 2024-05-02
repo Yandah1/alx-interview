a141d9450e1a80cdfe13e18147349059e2801bc4
Yandah1 <nonku003@gmail.com>
2024-05-03 00:01:17 +0200

    code modified

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,63 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics"""
-import sys
+"""Script that reads stdin line by line and computes metrics"""
 
-status_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
-                 '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-line_counter = 0
 
-try:
-    for line in sys.stdin:
-        line_list = line.split()
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            size = int(line_list[-1])
+def parseLog():
+    """
+    Parses logs from standard input and calculates
+    the file size and status codes.
+    """
+    import sys
 
-            if status_code in status_counts:
-                status_counts[status_code] += 1
+    line_counter = 0
+    total_size = 0
+    status_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                     '403': 0, '404': 0, '405': 0, '500': 0}
 
-            total_size += size
+    try:
+        for line in sys.stdin:
             line_counter += 1
+            line_list = line.split()
+            try:
+                size = int(line_list[-1])
+                status_code = line_list[-2]
 
-        if line_counter == 10:
-            line_counter = 0
-            print('Total file size:', total_size)
-            for code, count in sorted(status_counts.items()):
-                if count != 0:
-                    print('{}: {}'.format(code, count))
-            print()
+                if status_code in status_counts:
+                    status_counts[status_code] += 1
 
-except Exception as err:
-    print('An error occurred:', err)
+                total_size += size
+            except (IndexError, ValueError):
+                pass
 
-finally:
-    print('Total file size:', total_size)
+            if line_counter == 10:
+                report(total_size, status_counts)
+                line_counter = 0
+
+        report(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        report(total_size, status_counts)
+        raise
+    except BrokenPipeError:
+        # Handle the BrokenPipeError explicitly
+        pass
+
+
+def report(total_size, status_counts):
+    """
+    Prints generated report to standard output
+    Args:
+        total_size (int): total log size after every 10 successfully read lines
+        status_counts (dict): dictionary of status codes and counts
+    """
+    print("Total file size:", total_size)
     for code, count in sorted(status_counts.items()):
         if count != 0:
             print('{}: {}'.format(code, count))
+
+
+if __name__ == '__main__':
+    try:
+        parseLog()
+    except BrokenPipeError:
+        pass
